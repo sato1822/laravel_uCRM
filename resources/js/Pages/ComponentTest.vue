d09e07b74729dd9a046c60740974aaabd667324f
iori sato <iori1822@icloud.com>
2023-01-19 23:26:45 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,16 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import Label from '@/Components/InputLabel.vue';
+import Input from '@/Components/TextInput.vue'
+
+const emitTest = e => console.log(e)
+</script>
+<template>
+  <GuestLayout>
+    <Label value="aaaaaa">bbbbbbb</Label>
+    <Input 
+    modelValue="名前を記入してください"
+    @update:modelValue="emitTest"
+    ></Input>
+  </GuestLayout>
+</template>
